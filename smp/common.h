9a8f3874b8a745c7adae33779790d3ba0eba3c93
Zdeněk Janeček <jan.zdenek@gmail.com>
2015-10-21 21:13:14 +0200

    added thread state

--- smp/common.h
+++ smp/common.h
@@ -4,9 +4,14 @@
 
 #define ThreadTimeQuantum 52
 
+typedef enum test {
+	RUNNING, READY, BLOCKED, TERMINATED
+} EThreadState;
+
 typedef struct {
 	CONTEXT OSContext;
 	HANDLE StackHandle;
 	void* Stack;
 	int Id;
+	EThreadState state;
 } TThreadControlBlock;
