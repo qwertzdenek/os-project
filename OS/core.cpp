2d6f75be4d45cf67d106656e0918f9a2745aed14
Zdeněk Janeček <jan.zdenek@gmail.com>
2015-11-11 14:52:44 +0100

    added initial stack

--- OS/core.cpp
+++ OS/core.cpp
@@ -8,14 +8,14 @@ HANDLE core_handle[CORE_COUNT];
 DWORD WINAPI init_cpu_idle_task(void *param)
 {
 	while (true)
-		printf("Hello\n");
+		;
 
 	return 0;
 }
 
 void init_cpu_core(int core_number)
 {
-	core_handle[core_number] = (HANDLE)CreateThread(NULL, 0, init_cpu_idle_task, 0, 0, NULL);
+	core_handle[core_number] = (HANDLE)CreateThread(NULL, THREAD_STACK_SIZE, init_cpu_idle_task, 0, 0, NULL);
 	SetThreadAffinityMask(core_handle[core_number], 0x1 << core_number);
 }
 
