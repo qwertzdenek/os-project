b50c6d16239cdcbd92cba4c2f2f379abe537b02c
root <root@localhost>
2015-11-14 21:41:45 +0100

    corrected errors in semaphores

--- OS/sched_calls.cpp
+++ OS/sched_calls.cpp
@@ -24,25 +24,25 @@ int get_tid()
 }
 
 // release semaphore and return his value
-void semaphore_V(semaphore_t &s, int value)
+void semaphore_V(semaphore_t &s, int value) 
 {
     s._value += value;
 }
 
 // accuire semaphore and return his value
-int semaphore_P(semaphore_t &s, int value)
+int semaphore_P(semaphore_t &s, int value) 
 {
-	int expected;
-	int old;
+    int expected;
+    int old;
 
-	do {
-		do {
-			old = s._value;
-			expected = old - value;
-		} while (expected <= 0);
-	} while (!s._value.compare_exchange_weak(old, expected,
-		std::memory_order_release,
-		std::memory_order_relaxed));
+    do {
+        do {
+            old = s._value;
+            expected = old - value;
+        } while (expected < 0);
+    } while (!s._value.compare_exchange_weak(old, expected,
+            std::memory_order_release,
+            std::memory_order_relaxed));
 
-	return expected;
+    return expected;
 }
